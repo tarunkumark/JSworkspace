0007ba067c4e14a9f66d353e1f8720d9dfcd2283
tkksc <tkksctwo@gmail.com>
2020-11-27 19:41:53 +0530

    second commit

--- Practice/script.js
+++ Practice/script.js
@@ -16,4 +16,34 @@ class Particle {
         this.directionX = 1;
         this.directionY = 1;
     }
+    update(){
+        if(this.positionY > canvas.height) {
+            this.positionY= 0 - this.size;
+            this.weight=2;
+            this.positionX=Math.random()  * canvas.width;
+        }
+        this.weight+=0.05;
+        this.positionY+=this.weight;
+        this.positionX+=this.directionX;
+    }
+    draw(){
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.arc(this.positionX,this.positionY, this.size, 0, Math.PI *2);
+        ctx.closePath();
+        ctx.fill();
+    }
+
+
 }
+const particle1 = new Particle(400, 100);
+
+
+function animate(){
+    ctx.fillStyle='rgba(255,255,255,0.1)';
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    particle1.update();
+    particle1.draw();
+    requestAnimationFrame(animate);
+} 
+animate();
